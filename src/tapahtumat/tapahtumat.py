d60f7794469de9156284de86fe6ce1c124a73ef4
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-09-15 17:50:43 +0300

    tapahtumat-tiedosto luotu

---
+++ src/tapahtumat/tapahtumat.py
@@ -0,0 +1,23 @@
+import pygame
+
+
+class Tapahtumat:
+
+    # Luokka, joka tutkii ohjelman saamat syötteet.
+
+    def __init__(self):
+
+        # Määritellään pygame.
+
+        pygame.init()
+    
+    def tutki_tapahtumat(self):
+
+        # Tutkitaan tapahtumat.
+
+        for tapahtuma in pygame.event.get():
+            if tapahtuma.type == pygame.QUIT:
+                exit()
+
+
+tapahtumat = Tapahtumat()
