d17095e17a26477e6a234224f17596aead23b307
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-09-16 15:55:28 +0300

    kartta.py muutoksia

--- src/kartta/kartta.py
+++ src/kartta/kartta.py
@@ -4,6 +4,8 @@ class Kartta:
 
     def __init__(self):
         self.taulukko = [[0 for i in range(40)] for j in range(40)]
+        self.taulukko[0][0] = 1
+        self.taulukko[39][39] = 2
 
 
 kartta = Kartta()
