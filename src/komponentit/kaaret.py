d016919537636a8ee4ab550597e858ea71fe429a
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-09-16 15:56:12 +0300

    komponentit-tiedostot luotu

---
+++ src/komponentit/kaaret.py
@@ -0,0 +1,7 @@
+class Kaari:
+
+    # Luokka, joka määrittelee verkon kaarikomponenttien rakenteen.
+
+    def __init__(self, loppu, pituus):
+        self.loppu = loppu
+        self.pituus = pituus
---
+++ src/komponentit/solmut.py
@@ -0,0 +1,7 @@
+class Solmu:
+
+    # Luokka, joka määrittelee verkossa olevan solmun rakenteen.
+
+    def __init__(self, koordinaatit, etaisyys):
+        self.koordinaatit = koordinaatit
+        self.etaisyys = etaisyys
