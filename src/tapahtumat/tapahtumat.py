3ee932171730f66f09c4716b711e625fa2cb8125
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-09-23 15:07:38 +0300

    tapahtumat.py paivitys

--- src/tapahtumat/tapahtumat.py
+++ src/tapahtumat/tapahtumat.py
@@ -19,8 +19,6 @@ class Tapahtumat:
         for tapahtuma in pygame.event.get():
             if tapahtuma.type == pygame.QUIT:
                 exit()
-            if tapahtuma.type == pygame.MOUSEBUTTONDOWN:
-                return "click"
             if tapahtuma.type == pygame.KEYDOWN:
                 if tapahtuma.key == pygame.K_d:
                     tila.tila = 1
