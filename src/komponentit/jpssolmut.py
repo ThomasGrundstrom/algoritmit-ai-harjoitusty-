c0636e4e378cfa638e4dbfec9399949fea94b095
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-10-06 19:04:02 +0300

    jpssolmut.py paivityksia

--- src/komponentit/jpssolmut.py
+++ src/komponentit/jpssolmut.py
@@ -1,8 +1,12 @@
 class Jpssolmu:
+
+    # Määrittelee JPS:n käyttämien "solmujen" rakenteen.
+
     def __init__(self, koordinaatit, tyyppi):
         self.koordinaatit = koordinaatit
         self.tyyppi = tyyppi
         self.naapurit = []
+        self.luonnolliset = []
         self.pakolliset = []
         self.seuraajat = []
         self.edellinen = None
