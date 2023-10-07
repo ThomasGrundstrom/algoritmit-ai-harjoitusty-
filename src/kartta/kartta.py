4927b9fe97329455b6d627b85973deb940128aa6
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-10-07 17:52:05 +0300

    muutoksia

--- src/kartta/kartta.py
+++ src/kartta/kartta.py
@@ -5,7 +5,14 @@ class Kartta:
     def __init__(self):
         self.taulukko = [[0 for i in range(40)] for j in range(40)]
         self.taulukko[0][0] = 1
-        self.taulukko[39][4] = 2
+        self.taulukko[39][0] = 2
+#        for j in range(10):
+#            if j != 0:
+#                for i in range(39):
+#                    self.taulukko[j*4][i] = 3
+#        for j in [2, 6, 10, 14, 18, 22, 26, 30, 34, 38]:
+#            for i in range(1, 40):
+#                self.taulukko[j][i] = 3
         for i in range(39):
             self.taulukko[20][i] = 3
 
