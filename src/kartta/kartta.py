191e6f6ba4baa9fbe03f206146bbaf83db170e19
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-10-08 16:48:30 +0300

    kartta.py paivitys

--- src/kartta/kartta.py
+++ src/kartta/kartta.py
@@ -5,14 +5,32 @@ class Kartta:
     def __init__(self):
         self.taulukko = [[0 for i in range(40)] for j in range(40)]
         self.taulukko[0][0] = 1
-        self.taulukko[39][0] = 2
+        self.taulukko[39][39] = 2
+#        for j in range(50):
+#            if j != 0:
+#                for i in range(199):
+#                    self.taulukko[j*4][i] = 3
+#        for j in range(1, 49):
+#            for i in range(1, 200):
+#                self.taulukko[j*4+2][i] = 3
+
+#        for i in range(199):
+#            self.taulukko[20][i] = 3
+
 #        for j in range(10):
 #            if j != 0:
 #                for i in range(39):
 #                    self.taulukko[j*4][i] = 3
-#        for j in [2, 6, 10, 14, 18, 22, 26, 30, 34, 38]:
+
+#        for j in range(10):
 #            for i in range(1, 40):
-#                self.taulukko[j][i] = 3
+#                self.taulukko[j*4+2][i] = 3
+
+#        for j in range(20):
+#            if j != 0:
+#                for i in range(1, 39):
+#                    self.taulukko[j*2][i] = 3
+
         for i in range(39):
             self.taulukko[20][i] = 3
 
