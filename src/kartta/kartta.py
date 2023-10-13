21d493298c5bce342e0032fdc6694ff92a6c91fd
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-10-13 16:34:54 +0300

    kartta.py paivitys

--- src/kartta/kartta.py
+++ src/kartta/kartta.py
@@ -3,36 +3,43 @@ class Kartta:
     # Määrittelee kartan reitinhakua varten.
 
     def __init__(self):
-        self.taulukko = [[0 for i in range(40)] for j in range(40)]
+        self.taulukko = [[0 for i in range(180)] for j in range(100)]
         self.taulukko[0][0] = 1
-        self.taulukko[39][4] = 2
-#        for j in range(50):
+        self.taulukko[99][179] = 2
+
+#        for j in range(25):
 #            if j != 0:
-#                for i in range(199):
+#                for i in range(179):
 #                    self.taulukko[j*4][i] = 3
-#        for j in range(1, 49):
-#            for i in range(1, 200):
+#        for j in range(1, 24):
+#            for i in range(1, 180):
 #                self.taulukko[j*4+2][i] = 3
 
-#        for i in range(199):
+#        for i in range(179):
 #            self.taulukko[20][i] = 3
 
-#        for j in range(10):
+#        for j in range(25):
 #            if j != 0:
-#                for i in range(39):
+#                for i in range(179):
 #                    self.taulukko[j*4][i] = 3
 
-#        for j in range(10):
-#            for i in range(1, 40):
+#        for j in range(25):
+#            for i in range(1, 179):
 #                self.taulukko[j*4+2][i] = 3
+#        self.taulukko[98][0] = 3
 
-#        for j in range(20):
+#        for j in range(25):
 #            if j != 0:
-#                for i in range(1, 39):
+#                for i in range(1, 179):
 #                    self.taulukko[j*2][i] = 3
 
-        for i in range(39):
-            self.taulukko[20][i] = 3
+#        for i in range(39):
+#            self.taulukko[20][i] = 3
+
+#        for i in range(20, 98):
+#            self.taulukko[i][39] = 3
+
+
 
 
 kartta = Kartta()
