35622fbc65f2ccb9d48ee1963bc6aed28aba250e
Thomas Grundstrom <thomas.grundstrom@helsinki.fi>
2023-10-14 16:09:44 +0300

    dijkstra-testien muutoksia

--- src/testit/dijkstra_test.py
+++ src/testit/dijkstra_test.py
@@ -4,11 +4,97 @@ from algoritmit.dijkstra import Dijkstra
 
 class testDijkstra(unittest.TestCase):
     def setUp(self):
-        self.dijkstra = Dijkstra()
+        self.dijkstra = Dijkstra(None)
 
     def test_alkuarvot(self):
         self.assertEqual(self.dijkstra.solmut, [])
-        self.assertEqual(self.dijkstra.keko, [])
         self.assertEqual(self.dijkstra.alkusolmu, None)
         self.assertEqual(self.dijkstra.loppusolmu, None)
         self.assertEqual(self.dijkstra.polku, [])
+
+    def test_tapaukset(self):
+        kartta = self.nollaa_kartta()
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1093)
+        kartta = self.nollaa_kartta()
+        for j in range(25):
+            if j != 0:
+                for i in range(179):
+                    kartta[j*4][i] = 3
+        for j in range(1, 24):
+            for i in range(1, 180):
+                kartta[j*4+2][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 42272)
+        kartta = self.nollaa_kartta()
+        for i in range(179):
+            kartta[20][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1330)
+        kartta = self.nollaa_kartta()
+        kartta[99][179] = 0
+        kartta[99][0] = 2
+        for i in range(179):
+            kartta[20][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1988)
+        kartta = self.nollaa_kartta()
+        for j in range(25):
+            if j != 0:
+                for i in range(179):
+                    kartta[j*4][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1378)
+        kartta = self.nollaa_kartta()
+        kartta[99][179] = 0
+        kartta[99][0] = 2
+        for j in range(25):
+            if j != 0:
+                for i in range(179):
+                    kartta[j*4][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 2264)
+        kartta = self.nollaa_kartta()
+        for j in range(25):
+            for i in range(1, 179):
+                kartta[j*4+2][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1378)
+        kartta = self.nollaa_kartta()
+        for j in range(25):
+            if j != 0:
+                for i in range(1, 179):
+                    kartta[j*2][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1237)
+        kartta = self.nollaa_kartta()
+        for j in range(50):
+            for i in range(1, 179):
+                kartta[j*2][i] = 3
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1384)
+        kartta = self.nollaa_kartta()
+        for i in range(39):
+            kartta[20][i] = 3
+        for i in range(20, 98):
+            kartta[i][39] = 3
+        kartta[99][179] = 0
+        kartta[39][0] = 2
+        self.dijkstra = Dijkstra(kartta)
+        self.dijkstra.reitinhaku()
+        self.assertEqual(self.dijkstra.loppusolmu.etaisyys, 1005)
+
+    def nollaa_kartta(self):
+        kartta = [[0 for i in range(180)] for j in range(100)]
+        kartta[0][0] = 1
+        kartta[99][179] = 2
+        return kartta
